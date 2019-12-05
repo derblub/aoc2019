a4fa7edf5f6f7df1c44f2a04c205671420e76bb9
Daniel <daniel@pushingpixels.at>
2019-12-05 20:38:50 +0100

    cleaned up a little

--- 05_sunny-with-a-chance-of-asteroids/main.asm
+++ 05_sunny-with-a-chance-of-asteroids/main.asm
@@ -1,9 +1,10 @@
 
-; https://adventofcode.com/2019/day/5/answer
+; https://adventofcode.com/2019/day/5
 
 ; Most probably only compiles on 64bit linux. This is basically a hello-world program
 ; which outputs two lines with the correct puzzle answer to stdout, calling `write`
 ; then exists calling `exit`.
+;
 ; nasm -f elf64 -o main.o main.asm && ld -o main main.o && ./main
 
 
@@ -14,13 +15,13 @@ section .text
 _start:
   mov rax, 1        ; write(
   mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, out1     ;   "output ....\n",
+  mov rsi, out1     ;   "output: ...\n",
   mov rdx, out1len  ;   sizeof("output: ...\n")
   syscall           ; );
 
   mov rax, 1        ; write(
   mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, out2     ;   "output ....\n",
+  mov rsi, out2     ;   "output p2: ...\n",
   mov rdx, out2len  ;   sizeof("output p2: ...\n")
   syscall           ; );
 
@@ -29,7 +30,7 @@ _start:
   syscall           ; );
 
 section .rodata
-  out1: db "output: 14522484", 0x0a
+  out1: db "output: 14522484", 0x0a     ; 0x0a > \n
   out1len: equ $ - out1
   out2: db "output p2: 4655956", 0x0a
   out2len: equ $ - out2
