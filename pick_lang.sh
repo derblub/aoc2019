1c6d6ebe14c408facb7854607a159781a085978c
Daniel <daniel@pushingpixels.at>
2019-12-06 09:39:13 +0100

    todays language: erlang

--- pick_lang.sh
+++ pick_lang.sh
@@ -2,8 +2,8 @@
 
 # picks a random programming language and updates variables-list _in-file_
 
-LANGUAGES=( go c++ haskell scala kotlin ruby perl pascal  dart elixir rust haxe d erlang swift elm fortran nim cobol racket)
-DONE=(assembly php javascript bash python)
+LANGUAGES=( go c++ haskell scala kotlin ruby perl pascal  dart elixir rust haxe d  swift elm fortran nim cobol racket)
+DONE=(erlang assembly php javascript bash python)
 
 if [ ${#LANGUAGES[@]} -eq 0 ]; then
   echo "no more languages to pick from" && exit 1
