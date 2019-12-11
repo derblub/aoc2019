caf848d1f55a97c05a491b0185fe6056f4becff1
Daniel <daniel@pushingpixels.at>
2019-12-11 20:55:17 +0100

    refactored intcode computer.. it had to be done..

--- lib/intcode.py
+++ lib/intcode.py
@@ -1,155 +1,143 @@
 #!/usr/bin/env python3
 import argparse
 
-instructions = {
-    1: '_add',
-    2: '_multiply',
-    3: '_input',
-    4: '_output',
-    5: '_jump_if_true',
-    6: '_jump_if_false',
-    7: '_less_than',
-    8: '_equals',
-    9: '_adjust_base',
-}
 
+class IntCodeComputer(object):
+    def __init__(self, intcode, input_=None):
+        self.instructions = {
+            1: '_add',
+            2: '_multiply',
+            3: '_input',
+            4: '_output',
+            5: '_jump_if_true',
+            6: '_jump_if_false',
+            7: '_less_than',
+            8: '_equals',
+            9: '_adjust_base',
+        }
+        self.intcode = intcode  # intcode list
+        self.p = 0  # instruction pointer: address
+        self.parameter_mode = 0  # (0): positional || 1: immediate
+        self.relative_base = 0
+        self.input = input_ if input_ else []
+        self.wait_for_input = True
+        self.output = []
 
-def instruction_pointer_address(mem, index):
-    parameter_mode = (mem['parameter_mode'] // (10 ** (index - 1))) % 10
-    if parameter_mode == 0:
-        ip_address = mem['intcode'][mem['p'] + index]
-    if parameter_mode == 1:
-        ip_address = mem['p'] + index
-    if parameter_mode == 2:
-        ip_address = mem['relative_base'] + mem['intcode'][mem['p'] + index]
+    @staticmethod
+    def load_intcode(intcode_file):
+        with open(intcode_file, "r") as f:
+            data = f.read().replace('\n', '').replace(',', ' ')
+            return [int(op_code) for op_code in data.split(' ')]
 
-    try:
-        return ip_address
-    except NameError as e:
-        print(e)
+    def start_program(self):
+        self.step_program()
 
+    def step_program(self, input_=None):
+        self.input.extend(input_ if input_ else [])
+        self.wait_for_input = False
+        self.output = []
 
-def _allocate_address(mem, ip_address):
-    [mem['intcode'].extend([0]) for i in range(len(mem['intcode']), ip_address + 1)]
+        while True:
+            code = self.intcode[self.p]
+            op_code = code % 100
+            if op_code == 99:
+                break
+            elif op_code in self.instructions:
+                self.parameter_mode = code // 100
+                self.parse_instruction(self.instructions[op_code])
+                if self.wait_for_input:
+                    break
+            else:
+                raise ValueError("Error: op_code unknown: %s" % op_code)
+        return self.output.copy()
 
+    def parse_instruction(self, instruction_name):
+        instruction = getattr(self, instruction_name)
+        if not instruction:
+            raise NotImplementedError("Error: Instruction %s not implemented" % instruction_name)
+        return instruction()
 
-def _get(mem, index):
-    ip_address = instruction_pointer_address(mem, index)
-    return mem['intcode'][ip_address]
+    def instruction_pointer_address(self, index):
+        parameter_mode = (self.parameter_mode // (10 ** (index - 1))) % 10
+        if parameter_mode == 0:
+            ip_address = self.intcode[self.p + index]
+        if parameter_mode == 1:
+            ip_address = self.p + index
+        if parameter_mode == 2:
+            ip_address = self.relative_base + self.intcode[self.p + index]
 
+        try:
+            return ip_address
+        except NameError as e:
+            print(e)
 
-def _set(mem, index, value):
-    ip_address = instruction_pointer_address(mem, index)
-    if ip_address >= len(mem['intcode']):
-        _allocate_address(mem, ip_address)
-    mem['intcode'][ip_address] = value
+    def allocate_address(self, ip_address):
+        [self.intcode.extend([0]) for i in range(len(self.intcode), ip_address + 1)]
 
+    def _get(self, index):
+        ip_address = self.instruction_pointer_address(index)
+        try:
+            return self.intcode[ip_address]
+        except IndexError:
+            return 0
 
-def _add(mem):
-    _set(mem, 3, _get(mem, 1) + _get(mem, 2))
-    mem['p'] += 4
-    return mem
+    def _set(self, index, value):
+        ip_address = self.instruction_pointer_address(index)
+        if ip_address >= len(self.intcode):
+            self.allocate_address(ip_address)
+        self.intcode[ip_address] = value
 
+    def _add(self):
+        self._set(3, self._get(1) + self._get(2))
+        self.p += 4
 
-def _multiply(mem):
-    _set(mem, 3, _get(mem, 1) * _get(mem, 2))
-    mem['p'] += 4
-    return mem
+    def _multiply(self):
+        self._set(3, self._get(1) * self._get(2))
+        self.p += 4
 
-
-def _input(mem):
-    if len(mem['input']):
-        value = mem['input'].pop(0)
-    else:
-        value = int(input("Enter input: "))
-    _set(mem, 1, value)
-    mem['p'] += 2
-    return mem
-
-
-def _output(mem):
-    value = _get(mem, 1)
-    mem['output'].append(value)
-    mem['p'] += 2
-    return mem
-
-
-def _jump_if_true(mem):
-    if _get(mem, 1) != 0:
-        mem['p'] = _get(mem, 2)
-    else:
-        mem['p'] += 3
-    return mem
-
-
-def _jump_if_false(mem):
-    if _get(mem, 1) == 0:
-        mem['p'] = _get(mem, 2)
-    else:
-        mem['p'] += 3
-    return mem
-
-
-def _less_than(mem):
-    if _get(mem, 1) < _get(mem, 2):
-        _set(mem, 3, 1)
-    else:
-        _set(mem, 3, 0)
-    mem['p'] += 4
-    return mem
-
-
-def _equals(mem):
-    if _get(mem, 1) == _get(mem, 2):
-        _set(mem, 3, 1)
-    else:
-        _set(mem, 3, 0)
-    mem['p'] += 4
-    return mem
-
-
-def _adjust_base(mem):
-    mem['relative_base'] += _get(mem, 1)
-    mem['p'] += 2
-    return mem
-
-
-def load_intcode(intcode_file):
-    with open(intcode_file, "r") as f:
-        data = f.read().replace('\n', '').replace(',', ' ')
-        return [int(opcode) for opcode in data.split(' ')]
-
-
-def parse_instruction(instruction_name, mem):
-    possible = globals().copy()
-    possible.update(locals())
-    instruction = possible.get(instruction_name)
-    if not instruction:
-        raise NotImplementedError("Error: Instruction %s not implemented" % instruction_name)
-    return instruction(mem)
-
-
-def parse_intcode(intcode, input=[]):
-    mem = {  # memory
-        'intcode': intcode,   # intcode dict
-        'p': 0,               # instruction pointer: address
-        'parameter_mode': 0,  # (0): positional || 1: immediate
-        'input': input,
-        'output': [],
-        'relative_base': 0,
-    }
-    while mem['p'] < len(intcode):
-        code = mem['intcode'][mem['p']]
-        opcode = code % 100
-        if opcode == 99:
-            break
-        elif opcode in instructions:
-            mem['parameter_mode'] = code // 100
-            mem = parse_instruction(instructions[opcode], mem)
+    def _input(self):
+        if len(self.input):
+            value = self.input.pop(0)
         else:
-            raise ValueError("Error: opcode unknown: %s" % opcode)
+            self.wait_for_input = True
+            return
+        self._set(1, value)
+        self.p += 2
 
-    return mem
+    def _output(self):
+        value = self._get(1)
+        self.output.append(value)
+        self.p += 2
+
+    def _jump_if_true(self):
+        if self._get(1) != 0:
+            self.p = self._get(2)
+        else:
+            self.p += 3
+
+    def _jump_if_false(self):
+        if self._get(1) == 0:
+            self.p = self._get(2)
+        else:
+            self.p += 3
+
+    def _less_than(self):
+        if self._get(1) < self._get(2):
+            self._set(3, 1)
+        else:
+            self._set(3, 0)
+        self.p += 4
+
+    def _equals(self):
+        if self._get(1) == self._get(2):
+            self._set(3, 1)
+        else:
+            self._set(3, 0)
+        self.p += 4
+
+    def _adjust_base(self):
+        self.relative_base += self._get(1)
+        self.p += 2
 
 
 def main():
@@ -159,11 +147,11 @@ def main():
     params.add_argument("--verb")
     params.add_argument("--input")
     args = params.parse_args()
-    input = []
-    intcode = output = False
+    input_ = []
+    intcode = False
 
     if args.intcode:
-        intcode = load_intcode(args.intcode)
+        intcode = IntCodeComputer.load_intcode(args.intcode)
 
     if args.noun:
         intcode[1] = int(args.noun)
@@ -171,16 +159,14 @@ def main():
         intcode[2] = int(args.verb)
 
     if args.input:
-        input = [int(x) for x in args.input.split(' ')]
+        input_ = [int(x) for x in args.input.split(' ')]
 
-    if intcode:
-        try:
-            output = parse_intcode(intcode, input)
-            print(str(output))
-        except ValueError as e:
-            print(e)
-
-    return output
+    computer = IntCodeComputer(intcode, input_)
+    while computer.wait_for_input:
+        computer.start_program()
+        if computer.wait_for_input:
+            value = input("Enter input: ")
+            computer.step_program([int(value)])
 
 
 if __name__ == "__main__":
