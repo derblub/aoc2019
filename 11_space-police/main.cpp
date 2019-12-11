9d063847627abf1651e68cdb149082770c8cde40
Daniel <daniel@pushingpixels.at>
2019-12-11 23:42:27 +0100

    cleaned up. To be continued..

--- 11_space-police/main.cpp
+++ 11_space-police/main.cpp
@@ -10,37 +10,6 @@
 #include <unordered_map>
 
 
-PyObject *createClassObject(const char *name, PyMethodDef methods[]){
-
-    PyObject *pClassName = PyUnicode_FromString(name);
-    PyObject *pClassBases = PyTuple_New(0); // An empty tuple for bases is equivalent to `(object,)`
-    PyObject *pClassDic = PyDict_New();
-
-
-    PyMethodDef *def;
-    // add methods to class
-    for (def = methods; def->ml_name != NULL; def++)
-    {
-        printf("     add method %s\n", def->ml_name);
-        PyObject *func = PyCFunction_New(def, NULL);
-        PyObject *method = PyInstanceMethod_New(func);
-        PyDict_SetItemString(pClassDic, def->ml_name, method);
-        Py_DECREF(func);
-        Py_DECREF(method);
-    }
-
-    // pClass = type(pClassName, pClassBases, pClassDic)
-    PyObject *pClass = PyObject_CallFunctionObjArgs((PyObject *)&PyType_Type, pClassName, pClassBases, pClassDic, NULL);
-
-    Py_DECREF(pClassName);
-    Py_DECREF(pClassBases);
-    Py_DECREF(pClassDic);
-
-
-    return pClass;
-}
-
-
 int main() {
 
   // Set PYTHONPATH TO working directory
