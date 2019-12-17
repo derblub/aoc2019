04a3f7bf41af2e07ddfc339e6ba5b651864e20a6
Daniel <daniel@pushingpixels.at>
2019-12-17 13:39:13 +0100

    day 17 part 2 not working and I have no idea why..

--- 17_set-and-forget/main.py
+++ 17_set-and-forget/main.py
@@ -8,28 +8,16 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from lib.intcode import IntCodeComputer
 
 
-PATH = '#'
-SPACE = '.'
-NL = '\n'
-ROBOT = 'X^v<>'
-TILES = {
-    35: PATH,
-    46: SPACE,
-    10: NL,
-    94: ROBOT[:1],
-}
-
-
 def calibrate_cam(map_):
     ap_sum = 0
     for x, row in enumerate(map_[1:-1]):
         for y, tile in enumerate(row[1:-1]):
             if (
-                tile == PATH
-                and map_[x][y + 1] == PATH
-                and map_[x + 1][y] == PATH
-                and map_[x + 1][y + 2] == PATH
-                and map_[x + 2][y + 1] == PATH
+                tile == '#'
+                and map_[x][y + 1] == '#'
+                and map_[x + 1][y] == '#'
+                and map_[x + 1][y + 2] == '#'
+                and map_[x + 2][y + 1] == '#'
             ):
                 ap_sum += (x + 1) * (y + 1)
     return ap_sum
@@ -38,10 +26,14 @@ def calibrate_cam(map_):
 def render_grid(grid):
     tiles = []
     for tile in grid:
-        tiles.append(TILES[tile])
+        tiles.append(str(chr(tile)))
     output = "".join(tiles)
     print(output)
     return [[c for c in line] for line in output.splitlines()]
+
+
+def find_path(map_):
+    pass
 
 
 def main():
@@ -51,12 +43,31 @@ def main():
     map_ = render_grid(out)
     print("output: %d" % calibrate_cam(map_))
 
-    # while computer.wait_for_input:
-    #     out = computer.step_program()
-    #     if computer.wait_for_input:
-    #         value = input("Enter input: ")
-    #         out = computer.step_program([int(value)])
-    # return out
+    # manual path mapping + zipping
+    # L, 12, L, 8, R, 10, R, 10,        A
+    # L, 6, L, 4, L, 12,                B
+    # L, 12, L, 8, R, 10, R, 10,        A
+    # L, 6, L, 4, L, 12,                B
+    # R, 10, L, 8, L, 4, R, 10,         C
+    # L, 6, L, 4, L, 12,                B
+    # L, 12, L, 8, R, 10, R, 10,        A
+    # R, 10, L, 8, L, 4, R, 10,         C
+    # L, 6, L, 4, L, 12,                B
+    # R, 10, L, 8, L, 4, R, 10,         C
+
+    a = "L,12,L,8,R,10,R,10"
+    b = "L,6,L,4,L,12"
+    c = "R,10,L,8,L,4,R,10"
+    zipped = "A,B,A,B,C,B,A,C,B,C"
+    routine = zipped + "\n" + a + "\n" + b + "\n" + c + "\n" + "y\n"
+    input_ = [ord(x) for x in routine]
+    print(input_)
+
+    program[0] = 2
+    computer = IntCodeComputer(program, input_)
+    out = computer.start_program()
+    map_ = render_grid(out)
+    print(out)
 
 
 if __name__ == "__main__":
